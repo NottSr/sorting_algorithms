55dc5012934e8b7c2febe39fecf84300c0360c6c
NottSr <leonardo.br4@hotmail.com>
2022-06-18 15:03:19 -0500

    print array, print list and header files done, task 0 in progress

---
+++ 0-bubble_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/*
+ * bubble_sort - function that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ *
+ * @array: unsorted array of integers
+ * @size: size of array
+ *
+ * Return: void
+ **/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t idx = 0;
+}
