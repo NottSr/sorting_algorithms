ad29f78d3711dc6fed473a871dc23706b2af9628
NottSr <leonardo.br4@hotmail.com>
2022-06-21 00:56:47 -0500

    task 1 done

--- sort.h
+++ sort.h
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* Structures used */
 
@@ -27,5 +28,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
